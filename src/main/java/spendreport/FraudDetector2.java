d20974232ce2fbe7aa0b0db536f49853e70520cc
wangzihaogithub <842156727@qq.com>
2020-03-28 23:03:22 +0800

    学习flink

---
+++ src/main/java/spendreport/FraudDetector2.java
@@ -0,0 +1,112 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package spendreport;
+
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.util.Collector;
+import org.apache.flink.walkthrough.common.entity.Alert;
+import org.apache.flink.walkthrough.common.entity.Transaction;
+
+/**
+ * Skeleton code for implementing a fraud detector.
+ * 欺诈检测器 v2：状态 + 时间 = ❤️
+ *
+ * 骗子们在小额交易后不会等很久就进行大额消费，这样可以降低小额测试交易被发现的几率。
+ * 比如，假设你为欺诈检测器设置了一分钟的超时，对于上边的例子，交易 3 和 交易 4 只有间隔在一分钟之内才被认为是欺诈交易。
+ * Flink 中的 KeyedProcessFunction 允许您设置计时器，该计时器在将来的某个时间点执行回调函数。
+ *
+ */
+public class FraudDetector2 extends KeyedProcessFunction<Long, Transaction, Alert> {
+
+	private static final long serialVersionUID = 1L;
+
+	private static final double SMALL_AMOUNT = 1.00;
+	private static final double LARGE_AMOUNT = 500.00;
+	private static final long ONE_MINUTE = 60 * 1000;
+
+	private transient ValueState<Boolean> flagState;
+	private transient ValueState<Long> timerState;
+
+	@Override
+	public void open(Configuration parameters) {
+		ValueStateDescriptor<Boolean> flagDescriptor = new ValueStateDescriptor<>(
+				"flag",
+				Types.BOOLEAN);
+		flagState = getRuntimeContext().getState(flagDescriptor);
+
+		ValueStateDescriptor<Long> timerDescriptor = new ValueStateDescriptor<>(
+				"timer-state",
+				Types.LONG);
+		timerState = getRuntimeContext().getState(timerDescriptor);
+	}
+
+	@Override
+	public void processElement(
+			Transaction transaction,
+			Context context,
+			Collector<Alert> collector) throws Exception {
+
+		// Get the current state for the current key
+		Boolean lastTransactionWasSmall = flagState.value();
+
+		// Check if the flag is set
+		if (lastTransactionWasSmall != null) {
+			if (transaction.getAmount() > LARGE_AMOUNT) {
+				//Output an alert downstream
+				Alert alert = new Alert();
+				alert.setId(transaction.getAccountId());
+
+				collector.collect(alert);
+			}
+			// Clean up our state
+			cleanUp(context);
+		}
+
+		if (transaction.getAmount() < SMALL_AMOUNT) {
+			// set the flag to true
+			flagState.update(true);
+
+			long timer = context.timerService().currentProcessingTime() + ONE_MINUTE;
+			context.timerService().registerProcessingTimeTimer(timer);
+
+			timerState.update(timer);
+		}
+	}
+
+	@Override
+	public void onTimer(long timestamp, OnTimerContext ctx, Collector<Alert> out) {
+		// remove flag after 1 minute
+		timerState.clear();
+		flagState.clear();
+	}
+
+	private void cleanUp(Context ctx) throws Exception {
+		// delete timer
+		Long timer = timerState.value();
+		ctx.timerService().deleteProcessingTimeTimer(timer);
+
+		// clean up all state
+		timerState.clear();
+		flagState.clear();
+	}
+}
